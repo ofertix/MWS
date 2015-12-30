d3226e3463c45bc2d359dea490d6ab0c37fd545c
jcxk <juancarlos.escamilla@ofertix.com>
2015-12-30 11:03:15 +0100

    -fix bad naming

--- src/Mws/Model/AmazonFeedTypeTrait.php
+++ src/Mws/Model/AmazonFeedTypeTrait.php
@@ -19,7 +19,7 @@ trait AmazonFeedTypeTrait
             "Relationship" => "_POST_PRODUCT_RELATIONSHIP_DATA_",
             "Item" => "_POST_ITEM_DATA_",
             "ProductImage"=>"_POST_PRODUCT_IMAGE_DATA_",
-            "Pricing"=>"_POST_PRODUCT_PRICING_DATA_",
+            "Price"=>"_POST_PRODUCT_PRICING_DATA_",
             "Inventory" =>"_POST_INVENTORY_AVAILABILITY_DATA_",
             "Acknowledment" => "_POST_ORDER_ACKNOWLEDGEMENT_DATA_",
             "Fullfillment" => "_POST_ORDER_FULFILLMENT_DATA_",
