8b139ae33ee8e02079a877587f1a13c0df4b08bf
Sergio Susa <sergio.susa@ofertix.com>
2016-02-09 11:17:03 +0100

    agrego el composer lock para tener controladas las versiones que se usen o si se usan nuevas de projectos externos, agrego el git ignore para no subir cosas que no son y arreglo una mala praxis en la clase FeedClient

--- src/Mws/FeedClient.php
+++ src/Mws/FeedClient.php
@@ -569,7 +569,7 @@ HERE_DOC;
         $xmlFeed = $this->createXmlFeed($amazonOrders);
 
         /** @var  \MarketplaceWebService_Model_SubmitFeedResponse $response */
-        $response = $this->submitFeed($xmlFeed, $marketPlaceId, $amazonOrder);
+        $response = $this->submitFeed($xmlFeed, $marketPlaceId, array_pop($amazonOrders));
 
         $this->handleThrottling($response);
         /** @var AmazonRequest $amazonRequest */
