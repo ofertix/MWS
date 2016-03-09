e93390098dbfc8035ea5f7748d953fd25071e573
dani-softwarepadawan <jimenez.roque.d@gmail.com>
2016-03-09 10:27:12 +0100

    added relationship object

--- src/Mws/Model/AmazonRelationship.php
+++ src/Mws/Model/AmazonRelationship.php
@@ -18,8 +18,50 @@ class AmazonRelationship implements AmazonFeedTypeInterface
 
     const FEED_NAME = 'Relationship';
 
-    public function __construct()
-    {
+    /** @var  AmazonProduct */
+    private $parent;
+    /** @var  AmazonProduct[] */
+    private $children;
 
+    /**
+     * AmazonRelationship constructor.
+     * @param AmazonProduct $parent
+     * @param AmazonProduct[] $children
+     */
+    public function __construct(AmazonProduct $parent, array $children)
+    {
+        $this->parent = $parent;
+        $this->children = $children;
+    }
+
+    /**
+     * @return \SimpleXMLElement
+     */
+    public function xmlNode()
+    {
+        $rootNode = new \SimpleXMLElement('<'.$this->feedName().'></'.$this->feedName().'>');
+
+        $rootNode->addChild('ParentSKU', $this->parent->sku());
+
+        foreach ($this->children as $child) {
+            $this->addRelationNode($rootNode, $child);
+        }
+
+        return $rootNode;
+    }
+
+    /**
+     * @param \SimpleXMLElement $rootNode
+     * @param AmazonProduct $product
+     * @return \SimpleXMLElement
+     */
+    private function addRelationNode(\SimpleXMLElement $rootNode, AmazonProduct $product)
+    {
+        $relationNode = $rootNode->addChild('Relation');
+
+        $relationNode->addChild('SKU', $product->sku());
+        $relationNode->addChild('Type', 'Variation');
+
+        return $relationNode;
     }
 }
